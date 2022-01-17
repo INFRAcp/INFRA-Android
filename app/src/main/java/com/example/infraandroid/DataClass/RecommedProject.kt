5fb8322d7d3fe7141265a8f7f33151dadc458510
sodock00 <qordudal0303@gmail.com>
2022-01-17 17:47:45 +0900

    [feat] #15 Make Home View

--- app/src/main/java/com/example/infraandroid/DataClass/RecommedProject.kt
+++ app/src/main/java/com/example/infraandroid/DataClass/RecommedProject.kt
@@ -1,3 +1,9 @@
 package com.example.infraandroid.DataClass
 
-data class RecommedProject()
+data class RecommedProject(
+    var group: String? = "",
+    var name: String? = "",
+    var member: String? = "",
+    var state: String? = "",
+    var photo: Int? = null
+)
--- app/src/main/java/com/example/infraandroid/RVA/RecomProjectRVAdapter.kt
+++ app/src/main/java/com/example/infraandroid/RVA/RecomProjectRVAdapter.kt
@@ -1,4 +1,39 @@
 package com.example.infraandroid.RVA
 
-class RecomProjectRVAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.infraandroid.DataClass.RecommedProject
+import com.example.infraandroid.databinding.ActivityMainBinding
+import com.example.infraandroid.databinding.ItemProjectBinding
+
+class RecomProjectRVAdapter(private val recomprojectList: ArrayList<RecommedProject>):
+    RecyclerView.Adapter<RecomProjectRVAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int
+    ): RecomProjectRVAdapter.ViewHolder {
+        val binding: ItemProjectBinding = ItemProjectBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecomProjectRVAdapter.ViewHolder, position: Int) {
+        holder.bind(recomprojectList[position])
+    }
+
+    override fun getItemCount(): Int = recomprojectList.size
+
+    //뷰 홀더 준비
+    inner class ViewHolder(val binding: ItemProjectBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(recomproject: RecommedProject){
+            binding.itemProjectGroupTv.text = recomproject.group
+            binding.itemProjectNameTv.text = recomproject.name
+            binding.itemProjectMemberNumTv.text = recomproject.member
+            binding.itemProjectStateTv.text = recomproject.state
+            binding.itemRecommedProjectPhotoIv.setImageResource(recomproject.photo!!)
+
+        }
+    }
 }
