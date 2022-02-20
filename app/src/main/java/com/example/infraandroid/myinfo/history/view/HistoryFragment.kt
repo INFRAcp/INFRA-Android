85f10bce5977e63827512abc11f5904f865100f9
Seungmin-develop <tmdals8798@gmail.com>
2022-02-20 23:22:51 +0900

    [feat] #87 My Info Application Status & Scrap & History View

---
+++ app/src/main/java/com/example/infraandroid/myinfo/history/view/HistoryFragment.kt
@@ -0,0 +1,14 @@
+package com.example.infraandroid.myinfo.history.view
+
+import com.example.infraandroid.R
+import com.example.infraandroid.databinding.FragmentHistoryBinding
+import com.example.infraandroid.databinding.FragmentScrapBinding
+import com.example.infraandroid.util.BaseFragment
+
+class HistoryFragment :  BaseFragment<FragmentHistoryBinding>(R.layout.fragment_history){
+    override fun FragmentHistoryBinding.onCreateView(){
+    }
+
+    override fun FragmentHistoryBinding.onViewCreated(){
+    }
+}
