79f624bb5946b82faf3ecdc4185234239696cddc
Seungmin-develop <tmdals8798@gmail.com>
2022-03-03 21:46:47 +0900

    [feat] #97 Look All Project Server Connection

--- app/src/main/java/com/example/infraandroid/category/model/LookUpAllProjectService.kt
+++ app/src/main/java/com/example/infraandroid/category/model/LookUpAllProjectService.kt
@@ -3,10 +3,12 @@ package com.example.infraandroid.category.model
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface LookUpAllProjectService {
-    @GET("/project/inquiry")
+    @GET("/project/inquiry?")
     fun getLookUpAllProject(
-        @Header("user_id") user_id: String
+        @Header("X-ACCESS-TOKEN") jwt: String,
+        @Query("user_id") user_id : String
     ): Call<ResponseLookUpAllProjectData>
 }
--- app/src/main/java/com/example/infraandroid/category/view/adapter/IdeaListAdapter.kt
+++ app/src/main/java/com/example/infraandroid/category/view/adapter/IdeaListAdapter.kt
@@ -8,12 +8,12 @@ import com.bumptech.glide.Glide
 import com.example.infraandroid.util.ImageRound
 import com.example.infraandroid.R
 import com.example.infraandroid.category.model.IdeaListInfo
-import com.example.infraandroid.category.model.Result
+import com.example.infraandroid.category.model.ResponseLookUpAllProjectData
 import com.example.infraandroid.databinding.ItemIdeaListRecyclerviewBinding
 
 class IdeaListAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    val ideaList = mutableListOf<IdeaListInfo>()
+    var ideaList = mutableListOf<ResponseLookUpAllProjectData.Result>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = ItemIdeaListRecyclerviewBinding.inflate(
@@ -32,17 +32,17 @@ class IdeaListAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     inner class IdeaListViewHolder(
         private val binding:ItemIdeaListRecyclerviewBinding
     ): RecyclerView.ViewHolder(binding.root){
-        fun onBind(ideaListInfo: IdeaListInfo){
-            binding.projectCategoryTitle.text = ideaListInfo.projectName
-            binding.projectCategoryTextView.text = ideaListInfo.projectCategory
-            binding.hashTagOne.text = ideaListInfo.hashTagOne
-            binding.hashTagTwo.text = ideaListInfo.hashTagTwo
+        fun onBind(ideaListInfo: ResponseLookUpAllProjectData.Result){
+//            binding.projectCategoryTitle.text = ideaListInfo.
+//            binding.projectCategoryTextView.text = ideaListInfo.projectCategory
+//            binding.hashTagOne.text = ideaListInfo.hashTagOne
+//            binding.hashTagTwo.text = ideaListInfo.hashTagTwo
 
             ImageRound.roundAll(binding.projectImageView, 36f)
-
-            Glide.with(itemView)
-                .load(ideaListInfo.projectImg)
-                .into(binding.projectImageView)
+            binding.projectList  = ideaListInfo
+//            Glide.with(itemView)
+//                .load(ideaListInfo.projectImg)
+//                .into(binding.projectImageView)
             itemView.setOnClickListener {
                 it.findNavController().navigate(R.id.action_idea_list_fragment_to_categoryViewIdeaFragment)
             }
